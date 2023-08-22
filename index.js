5edf1e65f6ac0e96bdb93422f11d9b7d244643ed
unknown <stephenoluwafemi52@gmail.comm>
2023-08-22 03:23:11 +0100

    Done with assignment

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-// Write your code here!
-const rmv = document.getElementById("main");
-rmv.remove()
+// Create a new <h1> element with id 'victory'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
 
-const newHeader = document.getElementById("victory")
+// Set the text content of the new <h1> element
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Replace the existing 'main' element with the new <h1> element
+const main = document.getElementById('main');
+main.parentNode.replaceChild(newHeader, main);
+
+  
